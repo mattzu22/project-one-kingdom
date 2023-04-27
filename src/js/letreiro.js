03576166b1c42d18145e92406bea7d26983d1c46
Matheus Macedo <matheusg3.201168@gmail.com>
2023-04-26 22:46:53 -0300

    responsive in progress

--- src/js/letreiro.js
+++ src/js/letreiro.js
@@ -1,6 +1,7 @@
-const letreiroWrapper = document.querySelector('.letreiro-wrapper');
-const letreiroTexto = document.querySelectorAll('.letreiro-texto');
+// const letreiroWrapper = document.querySelector('.letreiro-wrapper');
+// const letreiroTexto = document.querySelectorAll('.letreiro-texto');
 
-letreiroWrapper.addEventListener('animationiteration', function() {
-  letreiroTexto.forEach(texto => texto.classList.toggle('invertido'));
-});
+// letreiroWrapper.addEventListener('animationiteration', function() {
+//   letreiroTexto.forEach(texto => texto.classList.toggle('invertido'));
+// });
+
